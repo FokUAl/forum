046ce65df5703e116473fb19a27394c1534b361a
Almas Serik <s.almas_99@mail.ru>
2022-10-15 21:38:07 +0600

    CreateCategory function added

--- database/category.go
+++ database/category.go
@@ -5,3 +5,8 @@ type Category struct {
 	Name string
 	Post []string
 }
+
+func CreateCategory(category_name string, post Post) (err error) {
+	_, err = database.Exec("INSERT INTO categories (name, post) values ($1, $2)", category_name, post)
+	return
+}
