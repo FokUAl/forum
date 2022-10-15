fda356750fb4181aa8a0d8de456ab424e650c085
Almas Serik <s.almas_99@mail.ru>
2022-10-15 18:44:39 +0600

    Updated Init function

--- database/core.go
+++ database/core.go
@@ -17,10 +17,23 @@ type Post struct {
 var database *sql.DB
 
 func Init() {
-	database, err := sql.Open("sqlite3", "./forum.db")
+	var err error
+	database, err = sql.Open("sqlite3", "./forum.db")
 	if err != nil {
 		panic("failed to connect database")
 	}
+
+	statement, _ := database.Prepare("CREATE TABLE IF NOT EXISTS users " +
+		"(id INTEGER PRIMARY KEY, firstname TEXT, lastname TEXT, email TEXT)")
+	statement.Exec()
+
+	statement, _ = database.Prepare("CREATE TABLE IF NOT EXISTS posts " +
+		"(id INTEGER PRIMARY KEY, message TEXT, author TEXT, email TEXT, rating INTEGER, category_id Integer)")
+	statement.Exec()
+
+	statement, _ = database.Prepare("CREATE TABLE IF NOT EXISTS users " +
+		"(id INTEGER PRIMARY KEY, firstname TEXT, lastname TEXT, email TEXT)")
+	statement.Exec()
 }
 
 func (post *Post) Create() (err error) {
