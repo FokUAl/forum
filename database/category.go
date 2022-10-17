06d5b84a719cbf934ca62031068271546aea7ad9
Almas Serik <s.almas_99@mail.ru>
2022-10-17 17:36:31 +0600

    Delete category function added

--- database/category.go
+++ database/category.go
@@ -10,3 +10,8 @@ func CreateCategory(category_name string, post Post) (err error) {
 	_, err = database.Exec("INSERT INTO categories (name, post) values ($1, $2)", category_name, post)
 	return
 }
+
+func DeleteCategory(category_id int) (err error) {
+	_, err = database.Exec("DELETE FROM categories WHERE id = $1", category_id)
+	return
+}
