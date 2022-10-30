da4794f97881e1f44d0df95c0e8820c8bcbc8745
fokual <alibek007007@gmail.com>
2022-10-31 01:00:35 +0600

    Reading data for sign up from front

--- web/handlers.go
+++ web/handlers.go
@@ -1,6 +1,8 @@
 package web
 
 import (
+	"forumAA/database"
+	"forumAA/internal"
 	"html/template"
 	"log"
 	"net/http"
@@ -61,22 +63,69 @@ func (app *application) post(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application) signUp(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodGet {
-		http.Error(w, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
-		w.WriteHeader(http.StatusMethodNotAllowed)
-		return
-	}
-
 	if r.URL.Path != "/signup" {
 		http.NotFound(w, r)
 		w.WriteHeader(http.StatusNotFound)
 		return
 	}
 
-	t, err := template.ParseFiles("./ui/template/signup.html")
+	switch r.Method {
+	case http.MethodGet:
+		t, err := template.ParseFiles("./ui/template/signUp.html")
+		if err != nil {
+			log.Println(err.Error())
+			http.Error(w, "File not found: signUp.html", 500)
+			return
+		}
+
+		err = t.Execute(w, nil)
+		if err != nil {
+			log.Println(err.Error())
+			http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+			return
+		}
+	case http.MethodPost:
+		err := r.ParseForm()
+		if err != nil {
+			http.Error(w, "StatusBadRequest", http.StatusBadRequest)
+			return
+		}
+
+		user := database.User{
+			Firstname: r.FormValue("firstName"),
+			Lastname:  r.FormValue("lastName"),
+			Email:     r.FormValue("email"),
+			Nickname:  r.FormValue("nickname"),
+			Password:  r.FormValue("password"),
+		}
+
+		err = internal.Registration(user)
+		if err != nil {
+			log.Println(err.Error())
+			http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		}
+	default:
+		http.Error(w, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
+	}
+}
+
+func (app *application) signIn(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		http.Error(w, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+
+	if r.URL.Path != "/signin" {
+		http.NotFound(w, r)
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	t, err := template.ParseFiles("./ui/template/signIn.html")
 	if err != nil {
 		log.Println(err.Error())
-		http.Error(w, "File not found: signup.html", 500)
+		http.Error(w, "File not found: signin.html", 500)
 		return
 	}
 
