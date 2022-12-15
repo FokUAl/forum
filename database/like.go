f858647e30d01b5c8bfdc1250faf67240d99d537
Almas Serik <s.almas_99@mail.ru>
2022-12-15 20:58:13 +0600

    UpdateCommentLike, CreateCommentLike, GetLikeByComment, GetCommentLikeByUser was created

--- database/like.go
+++ database/like.go
@@ -7,8 +7,8 @@ import (
 
 func GetLikeByPost(db *sql.DB, post_id int) ([]Like, error) {
 	result := []Like{}
-
-	rows, err := db.Query("SELECT id, nickname, like FROM post_likes WHERE post_id = $1")
+	statement := "SELECT id, nickname, like FROM post_likes WHERE post_id = $1"
+	rows, err := db.Query(statement, post_id)
 	if err != nil {
 		return nil, fmt.Errorf("GetPostLike: %w", err)
 	}
@@ -64,5 +64,68 @@ func CreatePostLike(db *sql.DB, nickname string, like int, post_id int) error {
 
 	defer stmt.Close()
 	err = stmt.QueryRow(nickname, like, post_id).Scan()
+	return err
+}
+
+func GetCommentLikeByUser(db *sql.DB, nickname string, comment_id int) (Like, error) {
+	result := Like{}
+
+	err := db.QueryRow("SELECT id, like FROM comment_likes "+
+		"WHERE nickname = $1 AND comment_id = $2", nickname,
+		comment_id).Scan(&result.Id, &result.Value)
+	if err != nil {
+		return result, fmt.Errorf("GetPostLikeByUser: %w", err)
+	}
+
+	result.Nickname = nickname
+	result.Elem_Id = comment_id
+
+	return result, nil
+}
+
+func GetLikeByComment(db *sql.DB, comment_id int) ([]Like, error) {
+	result := []Like{}
+
+	statement := "SELECT id, nickname, like FROM comment_likes WHERE comment_id = $1"
+	rows, err := db.Query(statement, comment_id)
+	if err != nil {
+		return nil, fmt.Errorf("GetLikeByComment: %w", err)
+	}
+
+	defer rows.Close()
+
+	for rows.Next() {
+		var like Like
+
+		err := rows.Scan(&like.Id, &like.Nickname, &like.Value)
+		if err != nil {
+			return nil, fmt.Errorf("GetPostLike: %w", err)
+		}
+
+		like.Elem_Id = comment_id
+
+		result = append(result, like)
+	}
+
+	return result, nil
+}
+
+func UpdateCommentLike(db *sql.DB, new_value int, nickname string, comment_id int) error {
+	_, err := db.Exec("UPDATE comment_likes SET like = $1 "+
+		"WHERE nickname = $2 AND comment_id = $3",
+		new_value, nickname, comment_id)
+
+	return err
+}
+
+func CreateCommentLike(db *sql.DB, nickname string, like int, comment_id int) error {
+	statement := "INSERT INTO comment_likes (nickname, like, comment_id) VALUES ($1, $2, $3)"
+	stmt, err := db.Prepare(statement)
+	if err != nil {
+		return err
+	}
+
+	defer stmt.Close()
+	err = stmt.QueryRow(nickname, like, comment_id).Scan()
 	return err
 }
