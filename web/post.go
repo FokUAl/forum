a31fd554bfaa1facc33d86a82634818cde50735d
Almas Serik <s.almas_99@mail.ru>
2022-12-24 17:48:29 +0600

    fixing bug with likePost inverse

--- web/post.go
+++ web/post.go
@@ -189,7 +189,7 @@ func (app *application) likePost(w http.ResponseWriter, r *http.Request) {
 	}
 
 	like, err := database.GetPostLikeByUser(app.database, user.Nickname, post_id)
-	if !errors.Is(err, sql.ErrNoRows) {
+	if err != nil && !errors.Is(err, sql.ErrNoRows) {
 		app.errorLog.Printf("likePost: %s\n", err.Error())
 		http.Error(w, http.StatusText(http.StatusInternalServerError),
 			http.StatusInternalServerError)
