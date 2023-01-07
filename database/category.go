6498224f10e4d9f2e37aca7eec514de2e5ce57c1
Almas Serik <s.almas_99@mail.ru>
2023-01-07 20:57:44 +0600

    Defer added

--- database/category.go
+++ database/category.go
@@ -5,23 +5,28 @@ import (
 	"fmt"
 )
 
+// Creates a row in the Category table for this "category" structure.
 func (category *Category) CreateCategory(db *sql.DB) (err error) {
 	stmt, err := db.Prepare("INSERT INTO categories (name, post_id) values ($1, $2) returning id")
 	err = stmt.QueryRow(category.Name, category.Post[0].Id).Scan(&category.Id)
 	return
 }
 
+// Deletes a row in the Category table for this "category" structure.
 func (category *Category) DeleteCategory(db *sql.DB) (err error) {
 	_, err = db.Exec("DELETE FROM categories WHERE id = $1", category.Id)
 	return
 }
 
+// Returns all categories of the post with the given ID.
 func GetAllCategoryByPost(db *sql.DB, id int) ([]string, error) {
 	statement := "SELECT name FROM categories WHERE post_id = $1"
 	rows, err := db.Query(statement, id)
 	if err != nil {
 		return nil, fmt.Errorf("get all category by post: %w", err)
 	}
+
+	defer rows.Close()
 
 	var result []string
 	for rows.Next() {
@@ -32,9 +37,13 @@ func GetAllCategoryByPost(db *sql.DB, id int) ([]string, error) {
 		result = append(result, category)
 	}
 
+	if rows.Err() != nil {
+	}
+
 	return result, nil
 }
 
+// Creates rows in the category table for given array of rows.
 func CreateCategories(db *sql.DB, categories []string, post_id int) error {
 	for _, category := range categories {
 		_, err := db.Exec("INSERT INTO categories (name, post_id) VALUES ($1, $2)", category, post_id)
